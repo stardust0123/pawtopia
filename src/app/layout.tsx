91a5f0960b0bfd69b1faf99288a0beedb00fecc1
TBNRGarret <104774231@student.swin.edu.au>
2026-01-24 08:14:25 +0700

    Change layout to improve responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} min-h-screen flex flex-col`}>
         <Nav />
-        <main className="flex-1 container mx-auto px-4 py-8">{children}</main>
+        <main className="flex-1 container mx-auto px-4 py-8 pt-20 md:pt-24">{children}</main>
         <Footer />
       </body>
     </html>
